2da704594f6c8bc9c26e332e0af9dfd855943be2
AaronL07796 <lia10@rpi.edu>
2025-11-07 17:20:43 -0500

    Added Cache folder from virtual machine
    added cache folder because some files inside are important (routes.rb)
    
    Co-Authored-By: 51syphus <170689324+51syphus@users.noreply.github.com>

---
+++ server/config/routes.rb
@@ -0,0 +1,27 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  resources :application
+  resources :books
+  resources :nanospace
+
+  get '/logged_in',	to: 'user_sessions#logged_in'
+  post '/login',	to: 'user_sessions#create'
+  delete '/logout',	to: 'user_sessions#destroy'
+  get '/park',	to: 'nanospace#park1'
+
+  post '/users', to: 'users#create'
+  get '/users', to: 'users#index'
+  get '/games/:id/high_scores', to: 'games#high_scores'
+  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "/csrf_token", to: "csrf#show"
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
